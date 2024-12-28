1d1106c3be4c0f4934e63ea66bc5d7182bed98ad
FeyFey <15575955950@163.com>
2024-12-28 16:10:38 +0800

    common folder

--- utils.py
+++
@@ -1,9 +0,0 @@
-# 图像预处理模块
-import cv2
-import numpy as np
-
-def preprocess_image(image):
-    image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
-    image_cv = cv2.medianBlur(image_cv, 3)
-    _, binary = cv2.threshold(image_cv, 150, 255, cv2.THRESH_BINARY)
-    return binary
